5db6ea26c7fbd792c238b18bbf002ca441cb4c0b
zengqi <qi.zeng1203@gmail.com>
2022-02-02 22:16:44 -0800

    Q1 done

--- HW1/Q1/src/edu/northeastern/qizeng/Main.java
+++ HW1/Q1/src/edu/northeastern/qizeng/Main.java
@@ -4,5 +4,43 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        String str = "abccba";
+        System.out.println(isPalindrome(str));
+
+    }
+
+    private static Boolean isPalindrome(String str) {
+        char[] arr = str.toCharArray();
+        int start  = 0 ;
+        int end = arr.length - 1;
+
+        while(start <= end){
+            if(arr[start] != arr[end]) {
+                System.out.println("It is NOT a Palindrome");
+                return false;
+            }
+            start ++;
+            end --;
+        }
+        System.out.println("It is a Palindrome");
+        return true;
+        /*int start = 0;
+        int num = str.length();
+        char[] oriArray = str.toCharArray(); // convert the string to array
+
+        char[] newArray = new char[num];
+        for (int i = num - 1; i >= 0; i--) {
+            newArray[i] = oriArray[num - i - 1]; //newList is the reversed list
+        }
+
+        for (int i = 0; i < num; i++) {
+            if(newArray[i] != oriArray[i]) {
+                System.out.println("It is NOT a Palindrome");
+                return false;
+            }
+        }
+        System.out.println("It is a Palindrome");
+        return true;*/
+
     }
 }
