4b780e671bd459d06d9b1346950b20c7038c6054
zengqi <qi.zeng1203@gmail.com>
2022-02-03 00:20:44 -0800

    Q4 done

--- HW1/Q4/src/edu/northeastern/qizeng/Main.java
+++ HW1/Q4/src/edu/northeastern/qizeng/Main.java
@@ -5,27 +5,25 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         String str1 = "aabbc";
-        String str2 = "caabc";
+        String str2 = "caabb";
         System.out.println(isAnagrams(str1, str2));
     }
 
     private static Boolean isAnagrams(String str1, String str2) {
-        int[] count1 = new int[52];// consider uppercase letter
-        int[] count2 = new int[52];
+        int[] count = new int[52];// consider uppercase letter
+        //int[] count2 = new int[52];
         char[] ch1 = str1.toCharArray();
         char[] ch2 = str2.toCharArray();
-        int num1 = str1.length();
-        int num2 = str2.length();
 
-        for(int i = 0; i < num1; i++) {
-            count1 [ch1[i]-'A'] ++;
+        for(int i = 0; i < str1.length(); i++) {
+            count [ch1[i]-'A'] ++;
         }
-        for(int i = 0; i < num2; i++) {
-            count2 [ch2[i]-'A'] ++;
+        for(int i = 0; i < str2.length(); i++) {
+            count [ch2[i]-'A'] --;
         }
 
-        for(int i = 0; i < count1.length; i++) {
-            if(count1[i] != count2[i]) {
+        for(int i = 0; i < count.length; i++) {
+            if(count[i] != 0) {
                 System.out.println("Two strings are not anagrams");
                 return false;
             }
