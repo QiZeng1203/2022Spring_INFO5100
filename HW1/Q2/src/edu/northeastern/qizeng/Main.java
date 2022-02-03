a9a5d1a38b339f51cb6458e018f993510e49a7b0
zengqi <qi.zeng1203@gmail.com>
2022-02-02 22:49:24 -0800

    Q2 done

--- HW1/Q2/src/edu/northeastern/qizeng/Main.java
+++ HW1/Q2/src/edu/northeastern/qizeng/Main.java
@@ -4,5 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        String str = "abcdea";
+        System.out.println(isUnique(str));
+
     }
+    private static Boolean isUnique(String str) {
+        char[] ch = str.toCharArray();
+        int num = str.length();
+        for (int i = 0; i < num; i++) {
+            for (int j = i+1; j < num; j++) {
+                if (ch[i] == ch[j]) {
+                    System.out.println("It is NOT a unique string");
+                    return false;
+                }
+            }
+        }
+        System.out.println("It is a unique string");
+        return true;
+    }
+
 }
