cf10797dfca5260e73cc4081d469c631231a64fa
zengqi <qi.zeng1203@gmail.com>
2022-02-02 23:27:54 -0800

    Q2 done

--- HW1/Q2/src/edu/northeastern/qizeng/Main.java
+++ HW1/Q2/src/edu/northeastern/qizeng/Main.java
@@ -4,15 +4,29 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        String str = "abcdea";
+        String str = "AaaBbCc";
         System.out.println(isUnique(str));
-
     }
+
     private static Boolean isUnique(String str) {
+        int[] count = new int[52]; // consider uppercase letter
         char[] ch = str.toCharArray();
         int num = str.length();
-        for (int i = 0; i < num; i++) {
-            for (int j = i+1; j < num; j++) {
+        for(int i = 0; i < num; i++) {
+            count [ch[i]-'A'] ++;
+        }
+        for(int i = 0; i < count.length; i++) {
+            if(count[i] > 1) {
+                System.out.println("It is NOT a unique string");
+                return false;
+            }
+        }
+        System.out.println("It is a unique string");
+        return true;
+        /*char[] ch = str.toCharArray();
+        int num = str.length();
+        for(int i = 0; i < num; i++) {
+            for(int j = i+1; j < num; j++) {
                 if (ch[i] == ch[j]) {
                     System.out.println("It is NOT a unique string");
                     return false;
@@ -20,7 +34,6 @@ public class Main {
             }
         }
         System.out.println("It is a unique string");
-        return true;
+        return true;*/
     }
-
 }
