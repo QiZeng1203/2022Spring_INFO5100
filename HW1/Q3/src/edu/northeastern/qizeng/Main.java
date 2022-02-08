d7a68ac540eb59c66ebba0da9ca10080e46aa8fc
zengqi <qi.zeng1203@gmail.com>
2022-02-07 18:23:38 -0800

    Q3 done

--- HW1/Q3/src/edu/northeastern/qizeng/Main.java
+++ HW1/Q3/src/edu/northeastern/qizeng/Main.java
@@ -4,5 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        String str = "AaBbCc";
+        System.out.println(isDuplicate(str));
+    }
+    private static boolean isDuplicate(String str) {
+        int[] count = new int[52];
+        char[] ch = str.toCharArray();
+        int num = str.length();
+        for(int i = 0; i < num; i++) {
+            count [ch[i]-'A'] ++;
+        }
+        for(int i = 0; i < count.length; i++) {
+            if(count[i] > 1 ) {
+                System.out.println("The string contains duplicates");
+                return true;
+            }
+        }
+        System.out.println("The string dose not contain duplicates");
+        return false;
     }
 }
