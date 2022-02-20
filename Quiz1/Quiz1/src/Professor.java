4c50ff82f40c3238cdf649b91cb91c7160e8d2fe
zengqi <qi.zeng1203@gmail.com>
2022-02-19 16:23:03 -0800

    Quiz 1

---
+++ Quiz1/Quiz1/src/Professor.java
@@ -0,0 +1,12 @@
+public class Professor extends Person {
+    private String giveQuiz;
+
+    public Professor() {
+        super();
+        this.giveQuiz = giveQuiz;
+    }
+
+    public void Professor (String giveQuiz) {
+        System.out.println("Professor is giving Quiz");
+    }
+}
---
+++ Quiz1/Quiz1/src/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+
+    public Student() {
+        super();
+    }
+
+    public void Student(String takeQuiz) {
+        System.out.println("Student is taking Quiz");
+    }
+}
